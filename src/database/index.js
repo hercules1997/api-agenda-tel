87fb1335894af131d4e74a4564f58cb90d4c31bd
hercules1997 <hercules.chaves.andrade@gmail.com>
2023-04-29 21:14:10 -0300

    criando conexão com banco de dados

---
+++ src/database/index.js
@@ -0,0 +1,27 @@
+import Sequelize from "sequelize"
+import User from "../app/models/User"
+
+import configDatabase from "../config/database"
+import Contact from "../app/models/Contact"
+
+const models = [User, Contact]
+
+/*
+ Método para iniciar a conecção do banco de dados
+*/
+class Database {
+  constructor() {
+    this.init()
+  }
+
+  init() {
+    this.connection = new Sequelize(configDatabase)
+    models
+      .map((model) => model.init(this.connection))
+      .map(
+        (model) => model.associate && model.associate(this.connection.models)
+      )
+  }
+}
+
+export default new Database()
