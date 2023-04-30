9045001b083350c2407d761d84eecf52f9be81ed
hercules1997 <hercules.chaves.andrade@gmail.com>
2023-04-29 21:15:44 -0300

    Criando migrate de contatos

---
+++ src/database/migrations/20230220131736-create-contacts.js
@@ -0,0 +1,50 @@
+"use strict"
+
+/*
+ Estrutura para criação da tabela de contatos no banco de dados
+*/
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("contacts", {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      ddd: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      phone: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      address: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    })
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable("contacts")
+  },
+}
