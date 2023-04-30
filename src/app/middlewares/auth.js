445ca660755d41bc43537760aae0e3832d56de4b
hercules1997 <hercules.chaves.andrade@gmail.com>
2023-04-29 21:18:10 -0300

    Criando autenticação de token

---
+++ src/app/middlewares/auth.js
@@ -0,0 +1,33 @@
+import jwt from "jsonwebtoken"
+import authConfig from "../../config/auth"
+
+/*
+  Estrutura de autenticação do token
+*/
+export default (request, response, next) => {
+  const authToken = request.headers.authorization
+
+  if (!authToken) {
+    return response.status(401).json({
+      error: "Token não fornecido",
+    })
+  }
+
+  const token = authToken.split(" ")[1]
+
+  try {
+    jwt.verify(token, authConfig.secret, function (err, decoded) {
+      if (err) {
+        throw new Error()
+      }
+      request.userId = decoded.id
+      request.userName = decoded.name
+
+      return next()
+    })
+  } catch (err) {
+    return response.status(401).json({
+      error: "Token é inválido",
+    })
+  }
+}
---
+++ src/config/auth.js
@@ -0,0 +1,8 @@
+
+/*
+ Chave token e tempo de duração
+*/
+export default {
+  secret: "43f4c7dd4a613b301427850c10339791",
+  expiresIn: "1d",
+}
