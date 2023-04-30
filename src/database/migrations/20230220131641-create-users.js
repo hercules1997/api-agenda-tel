3c166e7ee06af56e73b27be0551eb78a174cb88a
hercules1997 <hercules.chaves.andrade@gmail.com>
2023-04-29 21:15:10 -0300

    Criando migrate de usuário

---
+++ src/database/migrations/20230220131641-create-users.js
@@ -0,0 +1,45 @@
+"use strict"
+
+/*
+ Estrutura para criação da tabela de usuário no banco de dados
+*/
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("users", {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+      },
+
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    })
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable("users")
+  },
+}
